f51e690d6a7e27418d9bfc5b7cdf86b1ec1ec8f7
Sergey <example@gmail.com>
2025-03-02 21:14:46 +0300

    add new file

--- blyatscript.js
+++ blyatscript.js
@@ -2,3 +2,15 @@
 
 const result = confirm('are you here');
 console.log('hello world');
+/* команды для git:
+git init добавляет гит в папку с файлами 
+git status показывает в каком состоянии находятся файлы в папке:
+1. добавлены но не отслеживаются
+2. отслеживаются 
+3. созданы контрльные точки 
+git add -A отслеживать все файлы папки
+git add *.css *.js отслеживать все файлы с таким-то расширением 
+git config --local user.name "username" добавляет локально имя пользователя git 
+git config --local user.email example.@gmail.com добавляет локально почту пользователя git
+git commit -a -m "something saved here" добавляет контрльную точку ко всем файлам папки 
+git log позволяет увидеть все коммиты и кто их сделал 
